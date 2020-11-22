b3b186b4bfb3c78d5b15bcb6c84d6472c77e3d1d
Khan-Desktop <kxisystem441@gmail.com>
2020-11-21 20:54:42 -0500

    initial commit

---
+++ Runtime/Scripts/BoltDialogueUI.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Ludiq;
+using Yarn;
+using Yarn.Unity;
+
+namespace Yarn.Unity.Bolt 
+{ 
+    public class BoltDialogueUI : Yarn.Unity.DialogueUIBehaviour
+    {
+        private bool isRunningLine = false;                     // True when a line has been sent to be displayed by the dialogue runner and execution is paused.
+        private bool isRunningOptions = false;                  // True when options has been sent to be displayed by the dialogue runner and execution is paused.
+        private int optionsCount = 0;                           // Number of options sent to be displayed
+
+        private Action lineCompleteHandler;                     // Action supplied by RunLine to be triggered later
+        private Action<int> currentOptionSelectionHandler;      // Action supplied but RunOptions to be triggered later
+
+        /// <summary>
+        /// Runs a line supplied by the DialogueRunner. Refer to YarnSpinner docs for more information.
+        /// </summary>
+        public override Dialogue.HandlerExecutionType RunLine(Line line, ILineLocalisationProvider localisationProvider, Action onLineComplete)
+        {
+            isRunningLine = true;
+            lineCompleteHandler = onLineComplete;
+            string text = localisationProvider.GetLocalisedTextForLine(line);
+
+            // Trigger Bolt event with line information so it can be used in Bolt graphs.
+            CustomEvent.Trigger(this.gameObject, "RUN_LINE", text);
+
+            return Dialogue.HandlerExecutionType.PauseExecution;
+        }
+
+        /// <summary>
+        /// Runs Options supplied by the DialogueRunner. Refer to YarnSpinner docs for more information.
+        /// </summary>
+        public override void RunOptions(OptionSet optionSet, ILineLocalisationProvider localisationProvider, Action<int> onOptionSelected)
+        {
+            isRunningOptions = true;
+            currentOptionSelectionHandler = onOptionSelected;
+
+            List<string> OptionsText = new List<string>();
+            optionsCount = 0;
+
+            // Convert the options into a String List and count the number of options.
+            foreach (var optionString in optionSet.Options)
+            {
+                var optionText = localisationProvider.GetLocalisedTextForLine(optionString.Line);
+                OptionsText.Add(optionText);
+                optionsCount++;
+            }
+
+            // Trigger Bolt event with Options information
+            CustomEvent.Trigger(this.gameObject, "RUN_OPTIONS", OptionsText, optionsCount);
+        }
+
+        /// <summary>
+        /// Runs a Yarn Command triggered by the DialogueRunner. Refer to YarnSpinner docs for more information.
+        /// </summary>
+        public override Dialogue.HandlerExecutionType RunCommand(Command command, Action onCommandComplete)
+        {
+            // Trigger a Bolt event with the full command string
+            CustomEvent.Trigger(this.gameObject, "RUN_COMMAND", command.Text);
+
+            // Tell the Dialogue Runner to continue on and not wait for the command signal its completed
+            // Mirrors the functionality of the default DialogueUI
+            return Dialogue.HandlerExecutionType.ContinueExecution;
+        }
+
+        /// <summary>
+        /// Called when the dialogue system has started
+        /// </summary>
+        public override void DialogueStarted()
+        {
+            // Trigger Bolt event
+            CustomEvent.Trigger(this.gameObject, "DIALOGUE_STARTED");
+        }
+
+        /// <summary>
+        /// Called when the dialogue system has finished
+        /// </summary>
+        public override void DialogueComplete()
+        {
+            // Trigger Bolt event
+            CustomEvent.Trigger(this.gameObject, "DIALOGUE_COMPLETED");
+        }
+
+        /// <summary>
+        /// Called when a node has been completed
+        /// </summary>
+        public override Dialogue.HandlerExecutionType NodeComplete(string nextNode, Action onComplete)
+        {
+            // Trigger Bolt event with next node string
+            CustomEvent.Trigger(this.gameObject, "NODE_COMPLETED", nextNode);
+
+            // Tell the Dialogue Runner to continue on, we can't do much about actions in Bolt
+            return Dialogue.HandlerExecutionType.ContinueExecution;
+        }
+
+        /// <summary>
+        /// Called when the player wants to request the next step of the story.
+        /// To be used by something like a continue button.
+        /// </summary>
+        public void FinishLine() 
+        {
+            // If the method was called when it wasn't expected to
+            if (!isRunningLine) 
+            {
+                Debug.LogWarning("Finish Line method was called but no line is being displayed currently", this.gameObject);
+                return;
+            }
+
+            isRunningLine = false;
+
+            // Invoke the action with a safety null check
+            lineCompleteHandler?.Invoke();
+        }
+
+        /// <summary>
+        /// Called when an option is selected
+        /// To be used by something like the options buttons when selecting a dialogue choice
+        /// </summary>
+        public void SelectOption(int optionID)
+        {
+            // If the method was called when it wasn't expected to
+            if (!isRunningOptions) 
+            {
+                Debug.LogWarning("Select Option was called but no options are being displayed", this.gameObject);
+                return;
+            }
+
+            // If we get a possible invalid option ID, throw and error before it gets passed to the Dialogue Runner
+            if (optionID > optionsCount)
+            {
+                Debug.LogError("Select Option was called with an Option ID greater than the number of possible options, preventing option selection", this.gameObject);
+                return;
+            }
+
+            isRunningOptions = false;
+            optionsCount = 0;
+
+            // Invoke the action with a safety null check
+            currentOptionSelectionHandler?.Invoke(optionID);
+        }
+    }
+}
---
+++ Runtime/Scripts/BoltYarnSpinnerHelper.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Ludiq;
+using Yarn;
+using Yarn.Unity;
+
+namespace Yarn.Unity.Bolt 
+{
+    public class BoltYarnSpinnerHelper : MonoBehaviour
+    {
+        [Tooltip("Leave this on if you want the Helper to automatically create a Bolt Scene Variable called YS_BoltHelper, " +
+            "if you turn this off then make sure you have a system to get a reference to the Dialogue Runner in your graphs")]
+        public bool CreateSceneVariable = true;
+        [Tooltip("Stops the Helper from creating too many messages in the debug console, recomended to leave this off")]
+        public bool SuppressLogMessages = false;
+
+        [HideInInspector]
+        public DialogueRunner DialogueRunnerReference; // Hidden in the inspector, but scripts and graphs can access it
+
+        private void Awake()
+        {
+            // Attempt to find the Dialogue Runner on current game object
+            DialogueRunnerReference = GetComponent<DialogueRunner>();
+
+            if (DialogueRunnerReference == null)
+                Debug.LogError("The Bolt YarnSpinner Helper can't find the Dialogue Runner! Did you put me on the same game object as the DialogueRunner script?", this.gameObject);
+
+            // Create Bolt Scene variable
+            if (!Variables.ActiveScene.IsDefined("YS_BoltHelper") && CreateSceneVariable)
+            {
+                Variables.ActiveScene.Set("YS_BoltHelper", this.gameObject);
+
+                if (!SuppressLogMessages)
+                    Debug.Log("Bolt YarnSpinner Helper scene variable created successfully", this.gameObject);
+            }
+            else
+            {
+                if (!SuppressLogMessages)
+                    Debug.LogWarning("Bolt YarnSpinner Helper variable not created, make sure to edit GetYarnSpinnerComponets Super Unit to ensure everything still works", this.gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Subscribing to DialogueRunner Unity Events
+        /// Good practice when subscribing to Unity Events
+        /// </summary>
+        private void OnEnable()
+        {
+            if (DialogueRunnerReference != null)
+            {
+                DialogueRunnerReference.onNodeStart.AddListener(TriggerOnNodeStart);
+                DialogueRunnerReference.onNodeComplete.AddListener(TriggerOnNodeComplete);
+                DialogueRunnerReference.onDialogueComplete.AddListener(TriggerOnDialogueComplete);
+            }
+        }
+
+        /// <summary>
+        /// Unubscribing to DialogueRunner Unity Events
+        /// While normally this wouldn't be called in this application, its still good practice to do this
+        /// </summary>
+        private void OnDisable()
+        {
+            if (DialogueRunnerReference != null)
+            {
+                DialogueRunnerReference.onNodeStart.RemoveListener(TriggerOnNodeStart);
+                DialogueRunnerReference.onNodeComplete.RemoveListener(TriggerOnNodeComplete);
+                DialogueRunnerReference.onDialogueComplete.RemoveListener(TriggerOnDialogueComplete);
+            }
+        }
+
+        /// <summary>
+        /// Attatch to the default DialogueUI OnLineUpdate event to use it in Bolt graphs
+        /// </summary>
+        public void TriggerOnLineUpdate(string line) 
+        {
+            CustomEvent.Trigger(this.gameObject, "ON_LINE_UPDATE", line);
+        }
+
+        /// <summary>
+        /// Attatch to the default DialogueUI OnCommand event to use it in Bolt graphs
+        /// </summary>
+        public void TriggerOnCommand(string command) 
+        {
+            CustomEvent.Trigger(this.gameObject, "RUN_COMMAND", command);
+        }
+
+        /// <summary>
+        /// Automatically attatched by the helper to use the OnNodeStart Dialogue Runner events in Bolt graphs
+        /// </summary>
+        private void TriggerOnNodeStart(string nodeName) 
+        {
+            CustomEvent.Trigger(this.gameObject, "ON_NODE_START", nodeName);
+        }
+
+        /// <summary>
+        /// Automatically attatched by the helper to use the OnNodeComplete events Dialogue Runner in Bolt graphs
+        /// </summary>
+        private void TriggerOnNodeComplete(string nodeName)
+        {
+            CustomEvent.Trigger(this.gameObject, "ON_NODE_COMPLETE", nodeName);
+        }
+
+        /// <summary>
+        /// Automatically attatched by the helper to use the OnDialogueFinish events Dialogue Runner in Bolt graphs
+        /// </summary>
+        private void TriggerOnDialogueComplete()
+        {
+            CustomEvent.Trigger(this.gameObject, "ON_DIALOGUE_FINISH");
+        }
+    }
+}
